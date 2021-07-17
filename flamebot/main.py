984e5e24897bc243644c33b93e990c78f631a748
adrielmolerio <79387755+adrielmolerio@users.noreply.github.com>
2021-07-16 21:03:10 -0400

    Add files via upload
    Added an 8Ball feature. Just type $eightball and it should work.

--- flamebot/main.py
+++ flamebot/main.py
@@ -1,6 +1,7 @@
 import discord
 import os
 import re
+import random
 from dotenv import load_dotenv
 from discord.ext import commands
 
@@ -28,6 +29,14 @@ async def on_ready():
 @commands.guild_only()
 async def ping(ctx):
     await ctx.channel.send("Pong!")
+
+@bot.command()
+@commands.guild_only()
+async def eightball(ctx):
+    predictions = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes, definitely.", "You may rely on it.", "As I see it, yes.", "Most likely", "Outlook good.", 
+    "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.",
+    "My sources say no.", "Outlook not so good.", "Very doubtful."]
+    await ctx.channel.send(predictions[random.randint(1, 20)]) 
 
 @bot.event
 async def on_message(message):
